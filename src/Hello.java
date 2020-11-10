c550ba13ba3fe3d5812f7deb7234d95751a49bd3
hungnguyen <80joys@gmai.com>
2020-11-10 11:44:20 -0600

    add new intro

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello B21");
+        //preference=>gitHub=>add account gitHub.com
+        //VCS=>import=>share: gitHub.com
     }
 }
