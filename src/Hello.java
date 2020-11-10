ac3a06cf9782705d8aaf33eff70db27e0b2d00ed
hungnguyen <80joys@gmai.com>
2020-11-10 11:50:17 -0600

    add one line

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,12 @@ public class Hello {
         System.out.println("Hello B21");
         //preference=>gitHub=>add account gitHub.com
         //VCS=>import=>share: gitHub.com
+
+        // more stuff
+        // Mac user and windows users are happy to see commit dialog finally I have already published on Github.com
+        // now i am ready to make more commits
+        // but do I need to share again????
+        // ABSOLUTELY NO
+        // IT’S TIME COMMIT AND JUST PUSH!!!!!!
     }
 }
